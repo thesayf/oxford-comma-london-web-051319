42c5b9794f6c4168b69c9ab5641dbdadea66078d
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-06 19:02:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,10 @@
 def oxford_comma(array)
-  
- return array.join(", ")
-
+   if array.length < 2
+    array.join
+  elsif array.length == 2
+    array.insert(1, "and").join(" ")
+  else
+    str = ", and " + array.pop
+    str = array.join(", ") << str
+  end
 end
